62f9b2cafc2261b5903b3d3aadece8e9951eade3
ohb3fs <ohb3fs@virginia.edu>
2015-05-28 18:01:56 +0200

    fill_in_the_blank

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-	# create some validations here that ensure both our attributes
+	validates :bru_name, presence: true# create some validations here that ensure both our attributes
 	# are present when a user creates or edits a post
 end
